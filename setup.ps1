3059e963520c57297c371ccaa6ea8e1be6d6823a
currone <94299594+snackmasta@users.noreply.github.com>
2024-06-25 12:01:58 +0700

    Revert "inDev-0.0.2-protoDelete"
    This reverts commit be99807e4f06f147326dd1e8aec288abbeea5d0e.

--- setup.ps1
+++ setup.ps1
@@ -1,5 +1,18 @@
 Add-Type -AssemblyName System.Windows.Forms
 
+function Select-AppLocation {
+    $folderBrowser = New-Object System.Windows.Forms.FolderBrowserDialog
+    $folderBrowser.Description = "Select App Location"
+    $folderBrowser.ShowNewFolderButton = $true
+
+    $result = $folderBrowser.ShowDialog()
+    if ($result -eq [System.Windows.Forms.DialogResult]::OK) {
+        return $folderBrowser.SelectedPath
+    } else {
+        return $null
+    }
+}
+
 function Select-ProfileLocation {
     $folderBrowser = New-Object System.Windows.Forms.FolderBrowserDialog
     $folderBrowser.Description = "Select Profile Location"
@@ -13,6 +26,17 @@ function Select-ProfileLocation {
     }
 }
 
+$sourceLocation = Select-AppLocation
 $profileLocation = Select-ProfileLocation
 
-$profileLocation | Out-File -FilePath "C:\Program Files\currone\DesktopManager\data\directory.txt" -Encoding ascii
+if ($sourceLocation -and $profileLocation) {
+    # append the string to txt file
+    New-Item -ItemType Directory -Path (Join-Path -Path $sourceLocation -ChildPath "currone\DesktopManager\data")
+    $sourceFile = Join-Path -Path $sourceLocation -ChildPath "currone\DesktopManager\data\appDir.txt"
+    $profileFile = Join-Path -Path $profileLocation -ChildPath "currone\DesktopManager\data\deskDir.txt"
+    Add-Content -Path $sourceFile -Value $sourceLocation
+    Add-Content -Path $profileFile -Value $profileLocation
+
+} else {
+    Write-Output "No folder selected."
+}
