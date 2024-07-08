41105ac76ae180191f5691cee31811a0ae75bf63
currone <94299594+snackmasta@users.noreply.github.com>
2024-07-08 16:10:57 +0700

    inDev-0.0.8-FixLoadRegister

--- save-as.ps1
+++
@@ -1,4 +0,0 @@
-
-$deskData = 
-
-Start-Process -NoNewWindow -FilePath "robocopy" -ArgumentList '"C:\Users\Legion\Desktop" '$deskData' /E /COPY:DATS /R:0 /W:0 /XO /NFL /NDL /NJH /NJS /NS /NC /NP'
--- save.ps1
+++ save.ps1
@@ -41,7 +41,7 @@ Start-Process -NoNewWindow -FilePath "robocopy" -ArgumentList "E:\Obsidian\shigo
 
 # Write "done" to the console
 Write-Output "done"
-Start-Process "regedit.exe" -ArgumentList "/s", '"C:\Program Files\currone\DesktopManager\reg\$nameWithoutExtension.reg"' -NoNewWindow -Wait | Wait-Process
+regedit.exe /s '"C:\Program Files\currone\DesktopManager\reg\$nameWithoutExtension.reg"'
 Stop-Process -Name explorer -Force
 "@
 
