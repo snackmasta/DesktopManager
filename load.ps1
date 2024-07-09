a55bf63aaa82f9809f29e5d8d8af589bb642ce64
currone <94299594+snackmasta@users.noreply.github.com>
2024-07-09 15:20:50 +0700

    inDev-0.1.0-CombineSaveLoad

--- load.ps1
+++ load.ps1
@@ -1,3 +1,77 @@
+function Save-Profile {
+    # Define the path to search
+    $path = "C:\Users\Legion\Desktop"
+
+    # Define the file extension filter
+    $filter = "*.mbu"
+
+    # Get the list of .mbu files recursively and process each file
+    Get-ChildItem -Path $path -Filter $filter -Recurse | ForEach-Object {
+        # Extract the full name and name without extension
+        $fullName = $_.Name
+        $nameWithoutExtension = [System.IO.Path]::GetFileNameWithoutExtension($fullName)
+
+        # Start the robocopy process with the /MIR switch
+        $process = Start-Process -NoNewWindow -FilePath "robocopy" -ArgumentList "C:\Users\Legion\Desktop E:\Obsidian\shigoto\$nameWithoutExtension /MIR /COPY:DATS /R:0 /W:0 /XO /NFL /NDL /NJH /NJS /NS /NC /NP" -PassThru
+
+        # Wait for the process to complete
+        $process | Wait-Process
+
+        # Write "done" to the console
+        Write-Output "done"
+    }
+
+    $outputFilePath = "C:\Program Files\currone\DesktopManager\reg\$nameWithoutExtension.reg"
+
+    # Remove the file if it already exists
+    if (Test-Path $outputFilePath) {
+        Remove-Item $outputFilePath -Force
+    }
+
+    reg export "HKCU\SOFTWARE\Microsoft\Windows\Shell\Bags\1\Desktop" $outputFilePath
+
+# Define the content of the new .ps1 script
+$scriptContent = @"
+# clear desktop directory completely using rm -force
+Remove-Item -Path "C:\Users\Legion\Desktop" -Recurse -Force
+
+# Start the robocopy process with the /MIR switch
+
+# Wait for the process to complete
+Start-Process -NoNewWindow -FilePath "robocopy" -ArgumentList "E:\Obsidian\shigoto\$nameWithoutExtension C:\Users\Legion\Desktop /MIR /COPY:DATS /R:0 /W:0 /XO /NFL /NDL /NJH /NJS /NS /NC /NP" -PassThru | Wait-Process
+
+# Write "done" to the console
+Write-Output "done"
+regedit.exe /s '"C:\Program Files\currone\DesktopManager\reg\$nameWithoutExtension.reg"'
+Stop-Process -Name explorer -Force
+"@
+
+    # Write the content to the new .ps1 script file
+    $scriptContent | Out-File -FilePath "C:\Program Files\currone\DesktopManager\profiles\$nameWithoutExtension.ps1" -Encoding ascii
+    Invoke-PS2EXE "C:\Program Files\currone\DesktopManager\profiles\$nameWithoutExtension.ps1" "C:\Program Files\currone\DesktopManager\bin\$nameWithoutExtension.exe" -requireAdmin
+}
+
+function Get-Profile {
+    # Get the selected profile path
+    $selectedProfile = $profiles[$choiceInt - 1].Name
+
+    Write-Host "Selected profile: $selectedProfile"
+
+    # Remove the extension from the selected profile
+    $selectedProfileWithoutExtension = [System.IO.Path]::GetFileNameWithoutExtension($selectedProfile)
+
+    Write-Host "Selected profile without extension: $selectedProfileWithoutExtension"
+
+    # Add ".exe" extension to the selected profile
+    $selectedProfileWithoutExtension = $selectedProfileWithoutExtension + ".exe"
+
+    Write-Host "Selected profile without extension with .exe extension: $selectedProfileWithoutExtension"
+
+    # Run the selected profile
+    $path = "C:\Program Files\currone\DesktopManager\bin"
+    [System.Diagnostics.Process]::Start($path + "\\" + $selectedProfileWithoutExtension)
+}
+
 do {
     $profiles = Get-ChildItem "C:\Program Files\currone\DesktopManager\reg" | Sort-Object LastWriteTime -Descending
 
@@ -24,34 +98,18 @@ do {
     # Prompt the user to pick a number
     $choice = Read-Host -Prompt "Enter the number of the profile you want to choose"
 
-    # Validate the user's input by converting to integer
-    $choice = [int]$choice
+    # If the user input a number convert it to a integer If the user input a string convert it to a string
+    $choiceInt = [System.Convert]::ToInt32($choice)
+    $choiceString = $choice.ToString()
 
-    if ($choice -ge 1 -and $choice -le $profiles.Count) {
-        # Get the selected profile path
-        $selectedProfile = $profiles[$choice - 1].Name
-
-        Write-Host "Selected profile: $selectedProfile"
-
-        # Remove the extension from the selected profile
-        $selectedProfileWithoutExtension = [System.IO.Path]::GetFileNameWithoutExtension($selectedProfile)
-
-        Write-Host "Selected profile without extension: $selectedProfileWithoutExtension"
-
-        # Add ".exe" extension to the selected profile
-        $selectedProfileWithoutExtension = $selectedProfileWithoutExtension + ".exe"
-
-        Write-Host "Selected profile without extension with .exe extension: $selectedProfileWithoutExtension"
-
-        # Run the selected profile
-        $path = "C:\Program Files\currone\DesktopManager\bin"
-        [System.Diagnostics.Process]::Start($path + "\\" + $selectedProfileWithoutExtension)
-
+    if ($choiceInt -ge 1 -and $choiceInt -le $profiles.Count) {
+        Get-Profile
         # Clear console
         Clear-Host
-
-    } else {
-        Write-Host "Invalid selection. Please try again."
+    } elseif ($choiceString -eq "save") {
+        # Call the Find-Profile function
+        $profileNames = Save-Profile
+        Write-Host "Found profiles: $profileNames"
         # Clear console
         Clear-Host
     }
